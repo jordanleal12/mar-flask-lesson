8b9e7a0904147d0d14c097070bf816d2cdd3862c
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-07-19 10:48:03 +1000

    feat(Student): created model and defined schemas

---
+++ models/student.py
@@ -0,0 +1,19 @@
+from init import db
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    address = db.Column(db.String(100))
+
+
+class StudentSchema:
+    class meta:
+        model = Student
+        load_instance = True
+
+
+student_schema = StudentSchema()
+student_schema = StudentSchema(many=True)
